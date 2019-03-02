a4d2828bda50ea839d11a631cc670dec87960d67
baboucarr <baboucarrd43@gmail.com>
2019-03-02 22:42:24 +0000

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+function opennav() {
+    document.querySelector(".nav").style.left = '0';
+    document.querySelector(".header__close-btn").style.display = 'block';
+    document.querySelector(".menu-button").style.display  = 'none';
+}
+
+function closenav() {
+    //  var close = document.querySelector(".nav");
+
+    document.querySelector(".nav").style.left = "-1950px";
+    document.querySelector(".header__close-btn").style.display = "none";
+    document.querySelector(".menu-button").style.display = "block";
+}
